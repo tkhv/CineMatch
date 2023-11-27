c8f0f1fe1548c8e13f1f577da58f8992c7fe909c
Roham W <kusshu@kuss-air.lan>
2023-11-27 01:48:34 -0500

    test suite push

---
+++ frontend/src/tests/rating_functionality_individual.test.ts
@@ -0,0 +1,9 @@
+import {describe, expect, test} from '@jest/globals';
+import { render, waitFor, screen } from "@testing-library/react";
+
+// NOTE: to run tests we execute the command in the CLI: "npm run test".
+
+test("Rating Functionality (Individual)", () => {
+    // TODO: ensure that the rating functionality works (user can like/dislike, etc.)...
+    // NOTE: the expect() in JEST testing acts sort of AssertEqual in JUNIT testing...
+})
